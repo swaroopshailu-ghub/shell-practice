43c18ff95ed08211f031c2790172045bc6faf593
Swaroop <swaroop.atp@gmail.com>
2026-02-11 23:36:23 +0530

    welcome

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1: Hi $PERSON2! how are you?"
+echo "$PERSON2: hi $PERSON1! am doing good. Thanks"
+echo "$PERSON1: whats up"
+echo "$PERSON2: nothing. Good Bye"
