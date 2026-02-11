89dc59d933cbdf30bb9fdff028831d58b96ebade
Swaroop <swaroop.atp@gmail.com>
2026-02-11 23:44:48 +0530

    welcome

---
+++ variables2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$2
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2! how are you?"
+echo "$PERSON2: hi $PERSON1! am doing good. Thanks"
+echo "$PERSON1: whats up"
+echo "$PERSON2: nothing. Good Bye"
