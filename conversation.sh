4b421e703be0d4553d10cab0fc30c39037c61de1
Swaroop <swaroop.atp@gmail.com>
2026-02-11 23:31:40 +0530

    welcome

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "trump: Hi musk! how are you?"
+echo "musk: hi trump! am doing good. Thanks"
+echo "trump: whats up"
+echo "musk: nothing. Good Bye"
--- helloworld.sh
+++ helloworld.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-
-echo "trump: Hi musk! how are you?"
-echo "musk: hi trump! am doing good. Thanks"
-echo "trump: whats up"
-echo "musk: nothing. Good Bye"
+echo "hello world"
