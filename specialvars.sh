9a10b2c154d9ec03c04802c707274e45b8eacc07
Swaroop <swaroop.atp@gmail.com>
2026-02-12 23:45:43 +0530

    spec vars

--- specialvars.sh
+++ specialvars.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script $@"
 echo "Number of variables: $#"
+echo "Script Name: $0"
+echo "Current directory: $PWD"
+echo "Home directory of user: $HOME"
+echo "User running this script: $USER"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in backgrounf: $!"
