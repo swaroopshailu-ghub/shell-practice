c09c1831ffbef9bc64b83241e6d44a51fe0b7e1d
Swaroop <swaroop.atp@gmail.com>
2026-02-13 00:35:43 +0530

    spec vars

--- install-commands.sh
+++ install-commands.sh
@@ -7,20 +7,21 @@ then
     echo "ERROR: Please run this script with root user"
     exit 1
 else
-    echo "You're wuth root access"
+    echo "You are with root access"
 fi
 
 dnf list installed mqsql
+
 if [ $? -ne 0 ]
 then
     echo "MYSQL : going to install"
     dnf install mysql -y
     if [ $? -eq 0 ]
     then
-    echo "MQSQL installed Successfully"
+        echo "MQSQL installed Successfully"
     else
-    echo "FAILURE: MYSQL failed."
-    exit 1
+        echo "FAILURE: MYSQL failed."
+        exit 1
     fi
 else
     echo "MYSQL is already installed"
