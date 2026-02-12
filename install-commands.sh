a7daa3c3e2dc503d3fc199c3b0a6134d66c26296
Swaroop <swaroop.atp@gmail.com>
2026-02-13 00:10:39 +0530

    spec vars

--- install-commands.sh
+++ install-commands.sh
@@ -5,8 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR: Please run this script with root user"
+    exit 1
 else
     echo "You're wuth root access"
 fi
 
-# dnf install mysql -y
+dnf install mysql -y
