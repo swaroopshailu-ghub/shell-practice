0ef25762e39e4864eeedc8f6293998f0f1e6626b
Swaroop <swaroop.atp@gmail.com>
2026-04-02 23:54:09 +0530

    logs

--- funlogs.sh
+++ funlogs.sh
@@ -10,34 +10,34 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" &>>$LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-echo -e "$R ERROR: Please run with root access $N" &>>$LOG_FILE
+echo -e "$R ERROR: Please run with root access $N" | tee -a $LOG_FILE
 exit 1
 else
-echo -e "$G You are with root access $N" &>>$LOG_FILE
+echo -e "$G You are with root access $N" | tee -a $LOG_FILE
 fi
 
-dnf list installed mysql
+dnf list installed mysql &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-echo "MYSQL is going to install" &>>$LOG_FILE
+echo "MYSQL is going to install" | tee -a $LOG_FILE
 dnf install mysql -y
 VALIDATE $? "mysql"
 else
-echo -e "$Y MYSQL is already installed $N" &>>$LOG_FILE
+echo -e "$Y MYSQL is already installed $N" | tee -a $LOG_FILE
 fi
 
 dnf list installed nginx &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-echo "NGINX is going to install" &>>$LOG_FILE
+echo "NGINX is going to install" | tee -a $LOG_FILE
 dnf install nginx -y
 VALIDATE $? "nginx"
 else
-echo -e "$Y Nginx isalready installed $N" &>>$LOG_FILE
+echo -e "$Y Nginx isalready installed | tee -a $LOG_FILE
 fi
 
 
@@ -45,9 +45,9 @@ VALIDATE()
 {
     if [ $1 -eq 0 ]
     then
-    echo -e "$G $2 is successfully Installed $N" &>>$LOG_FILE
+    echo -e "$G $2 is successfully Installed $N" | tee -a $LOG_FILE
     else
-    echo -e "$R FAILURE: $2 is Failed. $N" &>>$LOG_FILE
+    echo -e "$R FAILURE: $2 is Failed. $N" | tee -a $LOG_FILE
     exit 1
     fi
 }
