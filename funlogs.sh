6ac6a8a9d8df9b57384f3e86dd7ca9dff1efd95b
Swaroop <swaroop.atp@gmail.com>
2026-04-02 23:27:06 +0530

    logs

---
+++ funlogs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(d -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+echo -e "$R ERROR: Please run with root access $N" &>>$LOG_FILE
+exit 1
+else
+echo -e "$G You are with root access $N" &>>$LOG_FILE
+fi
+
+dnf list installed mysql
+if [ $? =ne 0 ]
+then
+echo "MYSQL is going to install" &>>$LOG_FILE
+dnf install mysql -y
+VALIDATE $? "MySql"
+else
+echo -e "$Y MYSQL is already installed $N" &>>$LOG_FILE
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+echo "NGINX is going to install" &>>$LOG_FILE
+dnf install nginx -y
+VALIDATE $? nginx
+else
+echo -e "$Y Nginx isalready installed $N" &>>$LOG_FILE
+fi
+
+
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then
+    echo -e "$G $2 is successfully Installed $N" &>>$LOG_FILE
+    else
+    echo -e "$R FAILURE: $2 is Failed. $N" &>>$LOG_FILE
+    exit 1
+    fi
+}
